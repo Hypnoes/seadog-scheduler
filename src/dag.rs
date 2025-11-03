b4cc62aa39e53854cd7b50e2609d184c410fbe82
Hypnoes Liu <liumulei@icloud.com>
2025-11-03 17:27:39 +0000

    Rename Node to TaskNode and refactor Dag
    Add reverse_table and indegree maps and implement Kahn's topological
    sort. Replace traversal APIs (BFS/DFS/reset) with
    resolve_execution_order. Rename add_node/add_edge/get_nodes to
    add_task/add_task_relation/get_all_tasks. Change execute to return
    Result<(), String>. Update main and tests accordingly.

--- src/dag.rs
+++ src/dag.rs
@@ -1,30 +1,22 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 use std::fmt::Debug;
 use std::hash::{Hash, Hasher};
 
 pub type Task = fn() -> Result<(), String>;
 
-pub fn default_task() -> Result<(), String> {
-    Ok(())
-}
-
-pub struct Node {
+pub struct TaskNode {
     id: String,
     name: String,
     task: Task,
 }
 
-impl Node {
+impl TaskNode {
     pub fn new(name: String, task: Task) -> Self {
-        Node {
+        TaskNode {
             id: uuid::Uuid::new_v4().to_string(),
             name,
             task,
         }
-    }
-
-    pub fn set_name(&mut self, name: String) {
-        self.name = name;
     }
 
     pub fn execute(&self) -> Result<(), String> {
@@ -32,36 +24,23 @@ impl Node {
     }
 }
 
-impl PartialEq for Node {
+impl PartialEq for TaskNode {
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id
     }
-
-    fn ne(&self, other: &Self) -> bool {
-        !self.eq(other)
-    }
 }
 
-impl Eq for Node {}
+impl Eq for TaskNode {}
 
-impl Hash for Node {
+impl Hash for TaskNode {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.id.hash(state);
     }
-
-    fn hash_slice<H: std::hash::Hasher>(data: &[Self], state: &mut H)
-    where
-        Self: Sized,
-    {
-        for piece in data {
-            piece.hash(state)
-        }
-    }
 }
 
-impl Clone for Node {
+impl Clone for TaskNode {
     fn clone(&self) -> Self {
-        Node {
+        TaskNode {
             id: self.id.clone(),
             name: self.name.clone(),
             task: self.task.clone(),
@@ -69,33 +48,17 @@ impl Clone for Node {
     }
 }
 
-impl Default for Node {
-    fn default() -> Self {
-        Node {
-            id: uuid::Uuid::new_v4().to_string(),
-            name: String::new(),
-            task: default_task,
-        }
-    }
-}
-
-impl Debug for Node {
+impl Debug for TaskNode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Node").field("name", &self.name).finish()
+        write!(f, "Node({})", self.name)
     }
-}
-
-/// Enum representing the traversal order in a graph.
-///
-/// This enum specifies whether to traverse the graph in bfs or dfs order.
-enum Direction {
-    BFS,
-    DFS,
 }
 
 pub struct Dag {
     name: String,
-    node_table: HashMap<Node, Vec<Node>>,
+    node_table: HashMap<TaskNode, Vec<TaskNode>>,
+    reverse_table: HashMap<TaskNode, Vec<TaskNode>>,
+    indegree: HashMap<TaskNode, usize>,
 }
 
 impl Dag {
@@ -103,94 +66,46 @@ impl Dag {
         Dag {
             name,
             node_table: HashMap::new(),
+            reverse_table: HashMap::new(),
+            indegree: HashMap::new(),
         }
     }
 
-    pub fn add_node(&mut self, node: Node) {
-        self.node_table.insert(node, Vec::new());
+    pub fn add_task(&mut self, task_node: TaskNode) {
+        self.node_table.insert(task_node.clone(), Vec::new());
+        self.reverse_table.insert(task_node.clone(), Vec::new());
+        self.indegree.insert(task_node, 0);
     }
 
-    pub fn add_edge(&mut self, from: Node, to: Node) {
+    pub fn add_task_relation(&mut self, from: TaskNode, to: TaskNode) {
         if !self.node_table.contains_key(&from) {
             self.node_table.insert(from.clone(), Vec::new());
+            self.indegree.insert(from.clone(), 0);
         }
 
         if !self.node_table.contains_key(&to) {
             self.node_table.insert(to.clone(), Vec::new());
+            self.indegree.insert(to.clone(), 0);
         }
 
         self.node_table
-            .get_mut(&from)
-            .map(|neighbors| neighbors.push(to));
+            .entry(from.clone())
+            .or_insert(Vec::new())
+            .push(to.clone());
+
+        self.reverse_table
+            .entry(to.clone())
+            .or_insert(Vec::new())
+            .push(from.clone());
+
+        self.indegree
+            .entry(to.clone())
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
     }
 
-    pub fn get_nodes(&self) -> Vec<Node> {
+    pub fn get_all_tasks(&self) -> Vec<TaskNode> {
         self.node_table.keys().cloned().collect()
-    }
-
-    pub fn reset(&mut self) {
-        self.node_table.clear();
-    }
-
-    fn in_degree(&self, node: &Node) -> usize {
-        self.node_table
-            .values()
-            .filter(|neighbors| neighbors.contains(node))
-            .count()
-    }
-
-    /// Iterates over the nodes in the specified direction.
-    ///
-    /// Returns a vector of nodes in the specified direction.
-    fn traverse(&self, direction: Direction) -> Result<Vec<Node>, String> {
-        if self.node_table.is_empty() {
-            return Err("DAG is empty".to_string());
-        }
-
-        let mut queue: VecDeque<Node> = VecDeque::new();
-        let mut visited: HashSet<Node> = HashSet::new();
-        let mut result: Vec<Node> = Vec::new();
-
-        // initialize queue with all nodes with in-degree 0 (root nodes)
-        let mut in_degrees = HashMap::new();
-        for node in self.node_table.keys() {
-            in_degrees.insert(node.clone(), self.in_degree(node));
-        }
-        for node in self.node_table.keys() {
-            if in_degrees[&node] == 0 {
-                queue.push_back(node.clone());
-            }
-        }
-
-        // iterate over nodes in the specified direction
-        let pop = match direction {
-            Direction::BFS => VecDeque::pop_front,
-            Direction::DFS => VecDeque::pop_back,
-        };
-        while let Some(node) = pop(&mut queue) {
-            if !visited.contains(&node) {
-                visited.insert(node.clone());
-                result.push(node.clone());
-            }
-
-            if let Some(neighbors) = self.node_table.get(&node) {
-                for neighbor in neighbors {
-                    if !visited.contains(neighbor) {
-                        queue.push_back(neighbor.clone());
-                    }
-                }
-            }
-        }
-
-        Ok(result)
-    }
-
-    pub fn bfs(&self) -> Result<Vec<Node>, String> {
-        self.traverse(Direction::BFS)
-    }
-
-    pub fn dfs(&self) -> Result<Vec<Node>, String> {
-        self.traverse(Direction::DFS)
     }
 
     /// Topological sort of the DAG
@@ -202,51 +117,40 @@ impl Dag {
     /// Until the queue is empty, the nodes are added to the result vector.
     ///
     /// Returns a vector of nodes in topological order.
-    pub fn topo_sort(&self) -> Result<Vec<Node>, String> {
+    pub fn resolve_execution_order(&self) -> Result<Vec<TaskNode>, String> {
         if self.node_table.is_empty() {
             return Err("No nodes found".into());
         }
 
-        // Build indegree and adjacency (Kahn's algorithm)
-        let mut indegree: HashMap<&Node, usize> = HashMap::new();
-        let mut adj: HashMap<&Node, Vec<&Node>> = HashMap::new();
+        let mut result: Vec<TaskNode> = Vec::with_capacity(self.get_all_tasks().len());
 
-        // Fill indegree and adjacency maps
-        for (from, neighbors) in &self.node_table {
-            indegree.entry(from).or_insert(0);
-            let entry = adj.entry(from).or_insert_with(Vec::new);
-            for to in neighbors {
-                indegree.entry(to).and_modify(|d| *d += 1).or_insert(1);
-                entry.push(to);
-            }
-        }
-
-        // Ensure every node exists in adjacency map
-        let all_nodes: Vec<&Node> = indegree.keys().cloned().collect();
-        for n in all_nodes {
-            adj.entry(n).or_insert_with(Vec::new);
-        }
-
-        let mut queue: VecDeque<&Node> = indegree
+        // Initialize indegree map
+        let mut indegree: HashMap<&TaskNode, usize> = self
+            .indegree
             .iter()
-            .filter_map(|(n, &deg)| if deg == 0 { Some(*n) } else { None })
+            .map(|(node, &deg)| (node, deg))
             .collect();
 
-        let mut result: Vec<&Node> = Vec::with_capacity(indegree.len());
+        // Find all nodes with an indegree of 0 and add them to the queue
+        let mut queue: VecDeque<&TaskNode> = indegree
+            .iter()
+            .filter_map(|(&node, &deg)| if deg == 0 { Some(node) } else { None })
+            .collect();
 
-        // Fill result vector with topologically sorted nodes
-        while let Some(n) = queue.pop_front() {
-            result.push(n);
-            if let Some(neighbors) = adj.get(n) {
-                for m in neighbors {
-                    // Remove node from indegree map
-                    if let Some(d) = indegree.get_mut(m) {
+        // Start topological sort
+        while let Some(current_node) = queue.pop_front() {
+            // Clone only when pushing into the final result
+            result.push(current_node.clone());
+            if let Some(neighbors) = self.node_table.get(current_node) {
+                for neighbor in neighbors {
+                    // Remove current node (indegree = 0) and update indegree count
+                    if let Some(d) = indegree.get_mut(neighbor) {
                         if *d > 0 {
-                            // Decrement indegree count
+                            // Decrement indegree count (cause we removed current node)
                             *d -= 1;
                             if *d == 0 {
                                 // Add node to queue if indegree count reaches zero
-                                queue.push_back(m);
+                                queue.push_back(neighbor);
                             }
                         }
                     }
@@ -258,14 +162,16 @@ impl Dag {
             return Err("Graph has at least one cycle".into());
         }
 
-        Ok(result.into_iter().cloned().collect())
+        Ok(result)
     }
 
-    pub fn execute(&self) -> Vec<Result<(), String>> {
-        match self.topo_sort() {
+    pub fn execute(&self) -> Result<(), String> {
+        match self.resolve_execution_order() {
             Ok(task_order) => task_order.into_iter().map(|node| node.execute()).collect(),
             Err(err) => vec![Err(err)],
         }
+        .into_iter()
+        .try_for_each(|res| res)
     }
 }
 
@@ -288,102 +194,51 @@ mod tests {
 
     #[test]
     fn test_node_execute_ok() {
-        let n = Node::new("ok_task".to_string(), ok_task);
+        let n = TaskNode::new("ok_task".to_string(), ok_task);
         assert_eq!(n.execute().unwrap(), ());
     }
 
     #[test]
     fn test_node_execute_err() {
-        let n = Node::new("err_task".to_string(), err_task);
+        let n = TaskNode::new("err_task".to_string(), err_task);
         assert!(n.execute().is_err());
     }
 
     #[test]
     fn test_node_ops() {
         let mut dag = Dag::new("g".to_string());
-        let n = Node::new("n".to_string(), ok_task);
-        dag.add_node(n.clone());
-        let nodes = dag.get_nodes();
+        let n = TaskNode::new("n".to_string(), ok_task);
+        dag.add_task(n.clone());
+        let nodes = dag.get_all_tasks();
         assert_eq!(nodes.len(), 1);
-        dag.reset();
-        assert!(dag.get_nodes().is_empty());
     }
 
     #[test]
     fn test_edge_ops() {
         let mut dag = Dag::new("g".to_string());
-        let a = Node::new("a".to_string(), ok_task);
-        let b = Node::new("b".to_string(), ok_task);
-        dag.add_edge(a.clone(), b.clone());
-        assert_eq!(dag.get_nodes().len(), 2);
+        let a = TaskNode::new("a".to_string(), ok_task);
+        let b = TaskNode::new("b".to_string(), ok_task);
+        dag.add_task_relation(a.clone(), b.clone());
+        assert_eq!(dag.get_all_tasks().len(), 2);
         assert_eq!(dag.node_table.get(&a).unwrap().len(), 1);
         assert_eq!(dag.node_table.get(&b).unwrap().len(), 0);
-        dag.reset();
-        assert!(dag.get_nodes().is_empty());
-    }
-
-    #[test]
-    fn test_dag_bfs() {
-        let mut dag = Dag::new("g".to_string());
-        let a = Node::new("a".to_string(), ok_task);
-        let b = Node::new("b".to_string(), ok_task);
-        let c = Node::new("c".to_string(), ok_task);
-        let d = Node::new("d".to_string(), ok_task);
-        dag.add_edge(a.clone(), b.clone());
-        dag.add_edge(a.clone(), c.clone());
-        dag.add_edge(b.clone(), d.clone());
-        dag.add_edge(c.clone(), d.clone());
-        let bfs_result = dag
-            .bfs()
-            .unwrap()
-            .iter()
-            .map(|node| node.name.clone())
-            .collect::<Vec<_>>();
-        assert!(bfs_result == vec!["a", "b", "c", "d"] || bfs_result == vec!["a", "c", "b", "d"]);
-    }
-
-    #[test]
-    fn test_dag_dfs() {
-        let mut dag = Dag::new("g".to_string());
-        let a = Node::new("a".to_string(), ok_task);
-        let b = Node::new("b".to_string(), ok_task);
-        let c = Node::new("c".to_string(), ok_task);
-        let d = Node::new("d".to_string(), ok_task);
-        dag.add_edge(a.clone(), b.clone());
-        dag.add_edge(a.clone(), c.clone());
-        dag.add_edge(b.clone(), d.clone());
-        dag.add_edge(c.clone(), d.clone());
-        let dfs_result = dag
-            .dfs()
-            .unwrap()
-            .iter()
-            .map(|node| node.name.clone())
-            .collect::<Vec<_>>();
-        assert!(dfs_result == vec!["a", "b", "d", "c"] || dfs_result == vec!["a", "c", "d", "b"]);
-    }
-
-    #[test]
-    fn test_dag_iter_empty() {
-        let dag = Dag::new("g".into());
-        assert!(dag.bfs().is_err());
-        assert!(dag.dfs().is_err());
     }
 
     #[test]
     fn test_dag_topo_sort_ok() {
         let mut dag = Dag::new("g".into());
-        let a = Node::new("a".to_string(), ok_task);
-        let b = Node::new("b".to_string(), ok_task);
-        let c = Node::new("c".to_string(), ok_task);
-        let d = Node::new("d".to_string(), ok_task);
-        let e = Node::new("e".to_string(), ok_task);
-        dag.add_edge(a.clone(), b.clone());
-        dag.add_edge(a.clone(), c.clone());
-        dag.add_edge(c.clone(), d.clone());
-        dag.add_edge(b.clone(), e.clone());
-        dag.add_edge(d.clone(), e.clone());
+        let a = TaskNode::new("a".to_string(), ok_task);
+        let b = TaskNode::new("b".to_string(), ok_task);
+        let c = TaskNode::new("c".to_string(), ok_task);
+        let d = TaskNode::new("d".to_string(), ok_task);
+        let e = TaskNode::new("e".to_string(), ok_task);
+        dag.add_task_relation(a.clone(), b.clone());
+        dag.add_task_relation(a.clone(), c.clone());
+        dag.add_task_relation(c.clone(), d.clone());
+        dag.add_task_relation(b.clone(), e.clone());
+        dag.add_task_relation(d.clone(), e.clone());
         let order = dag
-            .topo_sort()
+            .resolve_execution_order()
             .unwrap()
             .iter()
             .map(|node| node.name.clone())
@@ -394,18 +249,18 @@ mod tests {
     #[test]
     fn test_dag_execute() {
         let mut dag = Dag::new("g".into());
-        let a = Node::new("a".to_string(), ok_task);
-        let b = Node::new("b".to_string(), ok_task);
-        let c = Node::new("c".to_string(), ok_task);
-        let d = Node::new("d".to_string(), ok_task);
-        let e = Node::new("e".to_string(), ok_task);
-        dag.add_edge(a.clone(), b.clone());
-        dag.add_edge(a.clone(), c.clone());
-        dag.add_edge(c.clone(), d.clone());
-        dag.add_edge(b.clone(), e.clone());
-        dag.add_edge(d.clone(), e.clone());
+        let a = TaskNode::new("a".to_string(), ok_task);
+        let b = TaskNode::new("b".to_string(), ok_task);
+        let c = TaskNode::new("c".to_string(), ok_task);
+        let d = TaskNode::new("d".to_string(), ok_task);
+        let e = TaskNode::new("e".to_string(), ok_task);
+        dag.add_task_relation(a.clone(), b.clone());
+        dag.add_task_relation(a.clone(), c.clone());
+        dag.add_task_relation(c.clone(), d.clone());
+        dag.add_task_relation(b.clone(), e.clone());
+        dag.add_task_relation(d.clone(), e.clone());
         let order = dag
-            .topo_sort()
+            .resolve_execution_order()
             .unwrap()
             .iter()
             .map(|node| node.name.clone())
@@ -413,6 +268,6 @@ mod tests {
         assert_eq!(order, vec!["a", "b", "c", "d", "e"]);
 
         let result = dag.execute();
-        assert_eq!(result, vec![Ok(()), Ok(()), Ok(()), Ok(()), Ok(())]);
+        assert_eq!(result, Ok(()));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod dag;
 
-use dag::{Dag, Node};
+use dag::{Dag, TaskNode};
 
 fn example_task_a() -> Result<(), String> {
     println!("Task A");
@@ -12,16 +12,18 @@ fn example_task_b() -> Result<(), String> {
     Ok(())
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     let mut dag = Dag::new("example_dag".to_string());
-    let task_a = Node::new("example_task_a".to_string(), example_task_a);
-    let task_b = Node::new("example_task_b".to_string(), example_task_b);
-    dag.add_edge(task_a.clone(), task_b.clone());
+    let task_a = TaskNode::new("example_task_a".to_string(), example_task_a);
+    let task_b = TaskNode::new("example_task_b".to_string(), example_task_b);
+    dag.add_task_relation(task_a.clone(), task_b.clone());
 
-    let results = dag.execute();
+    let result = dag.execute();
+
     match result {
-        Ok(()) => println!("Task completed successfully"),
-        Err(err) => println!("Task failed with error: {}", err),
+        Ok(_) => println!("All tasks completed successfully"),
+        Err(err) => println!("Error: {}", err),
     }
-    println!("Execution completed");
+
+    Ok(())
 }
